c0f522ed6d0aea8024299ce0454482b515257554
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-03-26 21:48:34 +0100

    adding admin path

--- main.go
+++ main.go
@@ -1,18 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer
+	path := http.Dir(".")
+	apiCfg := &apiConfig{}
+	fileHandler := http.StripPrefix("/app", fileServer(path))
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
+	mux.Handle("GET /app/", apiCfg.middlewareMetricsInc(fileHandler))
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
-	server.ListenAndServe()
+	log.Printf("Up and running on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
 
+func healthzHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	response.WriteHeader(http.StatusOK)
+	response.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(response http.ResponseWriter, request *http.Request) {
+	html := `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+	htmlWithCount := fmt.Sprintf(html, cfg.fileserverHits.Load())
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+	response.WriteHeader(http.StatusOK)
+	fmt.Fprint(response, htmlWithCount)
+}
+
+func (cfg *apiConfig) resetHandler(response http.ResponseWriter, request *http.Request) {
+	cfg.fileserverHits.Store(0)
+	response.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	response.WriteHeader(http.StatusOK)
+	response.Write([]byte("Metrics was reseted"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
