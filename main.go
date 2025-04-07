47010273c1b9954916b58d1fbb46cfa18e2bef7b
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-04-07 22:50:10 +0200

    adding connection to db

--- main.go
+++ main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Goralive/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Something go wrong", err)
+	}
+	dbQueries := database.New(db)
+
 	const port = "8080"
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer
 	path := http.Dir(".")
-	apiCfg := &apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
+	}
 	fileHandler := http.StripPrefix("/app", fileServer(path))
 
 	server := &http.Server{
