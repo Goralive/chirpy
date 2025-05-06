36a684c7ec0708897e4c1a1e5f19c1600d67c5c1
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-05-06 21:55:19 +0200

    handle handler_chirps_get

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetChirps(response http.ResponseWriter, request *http.Request) {
+	chirps := []Chirp{}
 	dbChirps, err := cfg.db.GetChirps(request.Context())
 	if err != nil {
 		respondWithError(response, http.StatusBadRequest, "Error during getting all the chirps", err)
 		return
 	}
-
-	var chirps []Chirp
 
 	for _, chirp := range dbChirps {
 		chirps = append(chirps, Chirp{
@@ -24,4 +25,28 @@ func (cfg *apiConfig) handlerGetChirps(response http.ResponseWriter, request *ht
 	}
 
 	respondWithJSON(response, http.StatusOK, chirps)
+}
+
+func (cfg *apiConfig) handlerGetChirp(response http.ResponseWriter, request *http.Request) {
+	chirpStringUuid := request.PathValue("chirpID")
+
+	chirpUuid, err := uuid.Parse(chirpStringUuid)
+	if err != nil {
+		respondWithError(response, http.StatusBadRequest, "Invalid chirp uuid", err)
+		return
+	}
+
+	dbChirp, err := cfg.db.GetChirp(request.Context(), chirpUuid)
+	if err != nil {
+		respondWithError(response, http.StatusNotFound, "Chirp Not found", err)
+		return
+	}
+
+	respondWithJSON(response, http.StatusOK, Chirp{
+		Id:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserId:    dbChirp.UserID,
+	})
 }
