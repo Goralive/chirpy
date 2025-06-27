d7dd0d7cbb78501c84edcd325e3097182e15390e
Igor Nosovskyi <nosovskyigor@gmail.com>
2025-06-27 21:47:58 +0200

    fix(sql): adjust sql

--- internal/database/refresh_tokens.sql.go
+++ internal/database/refresh_tokens.sql.go
@@ -15,8 +15,8 @@ import (
 const createRefreshToken = `-- name: CreateRefreshToken :one
 
 INSERT INTO refresh_tokens (
-    token, created_at, updated_at, user_id, expires_at, revoked_at
-) VALUES ($1, now(), now(), $2, $3, null)
+    token, created_at, updated_at, user_id, expires_at
+) VALUES ($1, now(), now(), $2, $3)
 RETURNING token, created_at, updated_at, user_id, expires_at, revoked_at
 `
 
--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -1,6 +1,6 @@
 -- name: CreateRefreshToken :one
 
 INSERT INTO refresh_tokens (
-    token, created_at, updated_at, user_id, expires_at, revoked_at
-) VALUES ($1, now(), now(), $2, $3, null)
+    token, created_at, updated_at, user_id, expires_at
+) VALUES ($1, now(), now(), $2, $3)
 RETURNING *;
